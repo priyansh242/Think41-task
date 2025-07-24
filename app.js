64121667916c02b6a20512cdea18dd76c4c15be7
priyansh242 <142443657+priyansh242@users.noreply.github.com>
2025-07-25 00:16:11 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,25 @@
+// app.js
+const express = require('express');
+const app = express();
+const taskRoutes = require('./routes/tasks');
+
+app.use(express.json());
+
+// Routes
+app.use('/tasks', taskRoutes);
+
+// 404 handler
+app.use((req, res, next) => {
+  res.status(404).json({ message: 'Route not found' });
+});
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ message: 'Something went wrong!' });
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Task Scheduler API running on http://localhost:${PORT}`);
+});
